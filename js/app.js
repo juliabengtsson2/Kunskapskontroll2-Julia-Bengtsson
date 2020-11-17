9e540c24544bca2678dcbf2b87f085f88f66e409
julia.bengtsson1 <julia.bengtsson@utb.ecutbildning.se>
2020-11-17 18:24:50 +0100

    changes

--- js/app.js
+++ js/app.js
@@ -24,17 +24,37 @@ formWeather.addEventListener('submit',
         // Lägger in min URL 
         let url = `https://api.openweathermap.org/data/2.5/weather?q=${inputCity}&units=metric&appid=${apiKey}`;
 
-        
-        // Description
-        let descriptionBoxText = document.querySelector('.discription');
-        descriptionBoxText.innerText = data.weather[0].description;
+        fetch(url).then(
+            function(response){
+                return response.json();
+            }
+        ).then(
+            function(data){
+                console.log(data)
+                // Description
+                let descriptionBoxText = document.querySelector('.descriprion h6');
+                descriptionBoxText.innerText = data.weather[0].description;
  
-        // Weather-icon
-        let weatherIcon = document.querySelector('.weather-icon');
-        let iconImg = data.weather[0].icon;
-        let iconUrl = `http://openweathermap.org/img/wn/${iconImg}@2x.png`;
+                // Weather-icon
+                let weatherIcon = document.querySelector('.weather-icon img');
+                let iconImg = data.weather[0].icon;
+                let iconUrl = `http://openweathermap.org/img/wn/${iconImg}@2x.png`;
  
-        weatherIcon.src = iconUrl;
+                weatherIcon.src = iconUrl;
+
+                // Tempratur
+                let tempration = document.querySelector('.tempration h6');
+                tempration.innerText = data.weather[0].tempration; // UNDEFINED
+
+                // Wind
+                let wind = document.querySelector('.wind h6');
+                wind.innerText = data.weather[0].wind; // Undefined
+
+                // Luktfuktigheten
+                let luftfuktigheten = document.querySelector('.humidity h6');
+
+            }
+        )
 
 }
 );
