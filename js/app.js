93b447d7e644ea6a0c5d985a0919bf7082836d4b
julia.bengtsson1 <julia.bengtsson@utb.ecutbildning.se>
2020-11-18 15:46:03 +0100

    errorhantering

--- js/app.js
+++ js/app.js
@@ -24,37 +24,66 @@ formWeather.addEventListener('submit',
         // Lägger in min URL 
         let url = `https://api.openweathermap.org/data/2.5/weather?q=${inputCity}&units=metric&appid=${apiKey}`;
 
+
         fetch(url).then(
             function(response){
+                console.log(response);
                 return response.json();
+                changeColor();
             }
         ).then(
             function(data){
                 console.log(data)
+
                 // Description
-                let descriptionBoxText = document.querySelector('.descriprion h6');
+                //Selectar mina element och letar fram data för description.
+                let descriptionBoxText = document.querySelector('.description h6');
                 descriptionBoxText.innerText = data.weather[0].description;
  
                 // Weather-icon
+                // Selectar mina element, letar upp data för iconen. Lägger till en url för iconerna . 
                 let weatherIcon = document.querySelector('.weather-icon img');
                 let iconImg = data.weather[0].icon;
                 let iconUrl = `http://openweathermap.org/img/wn/${iconImg}@2x.png`;
- 
                 weatherIcon.src = iconUrl;
 
-                // Tempratur
+                // Temperatur
+                // Selectar elementen och letar fram data för temperaturen. Avrundar graderna till ett heltal.
                 let tempration = document.querySelector('.tempration h6');
-                tempration.innerText = data.weather[0].tempration; // UNDEFINED
+                let tempBox = data.main.temp;
+                let tempRound = Math.round(tempBox);
+                tempration.innerText = `${tempRound} celsius `;
 
-                // Wind
+                // Vindhastigheten
+                // Selectar elementen och letar fram data för vindhastigheten
                 let wind = document.querySelector('.wind h6');
-                wind.innerText = data.weather[0].wind; // Undefined
+                wind.innerText = `${data.wind.deg} m/s `;
 
                 // Luktfuktigheten
+                // Selectar elementen och letar fram data för luktfuktigheten
                 let luftfuktigheten = document.querySelector('.humidity h6');
+                luftfuktigheten.innerText = `${data.main.humidity} %`;
 
+                //Timezone
+                // Selectar mina element och letar fram data för timezone.
+                let timeZone = document.querySelector('.timezone h6');
+                timeZone.innerText = data.timezone;
+
+                
+            }
+        ).catch(
+            function(error){
+                alert('Staden kunde inte hittas, försök igen');
             }
         )
 
 }
 );
+
+function changeColor(){
+    let wind = document.querySelector('.wind');
+
+    if(tempRound <= 20){
+        wind.style.backgroundColor = 'ljusblå';
+    }
+}
